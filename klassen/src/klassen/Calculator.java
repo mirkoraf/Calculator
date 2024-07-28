4b41fad2229f54c68f54d9c6f45d886a6cee0cd9
mirkoraf <rafailovic.mirko@gmail.com>
2024-07-28 11:11:04 +0200

    fixed exit condition!

--- klassen/src/klassen/Calculator.java
+++ klassen/src/klassen/Calculator.java
@@ -1,21 +1,45 @@
 package klassen;
 
 public class Calculator {
-	
+
 	Double firstNumber;
 	Double secondNumber;
-	String operator;
-	
+	// String operator;
+	// String calcExit;
+	// String exitVar;
+
 	public Calculator(Double firstNumber, Double secondNumber) {
 		this.firstNumber = firstNumber;
 		this.secondNumber = secondNumber;
-		
-		
-		
+	
+
 	}
 
 	public Double addition(Double firstNumber, Double secondNumber) {
 		return firstNumber + secondNumber;
 	}
 
+	public Double subtraction(Double firstNumber, Double secondNumber) {
+		return firstNumber - secondNumber;
+	}
+
+	public Double multiplication(Double firstNumber, Double secondNumber) {
+		return firstNumber * secondNumber;
+	}
+
+	public Double division(Double firstNumber, Double secondNumber) {
+		return firstNumber / secondNumber;
+	}
+
+	/* public Boolean exitCalc(String exitVar) {
+		System.out.println(exitVar);
+		if (this.exitVar == "Y") {
+			
+			return false;
+		} else {
+			return true;
+		}
+
+	}
+ */
 }
--- klassen/src/klassen/Main.java
+++ klassen/src/klassen/Main.java
@@ -8,36 +8,55 @@ public static void main(String[] args) {
 		
 	Double firstNumber;
 	Double secondNumber;
-	String operator ="",
-	
-	
-	Calculator calculator = new Calculator(0.0 , 0.0);
+	String operator = "";
+	Boolean calcRun = true;
+	String exitVar = "";
+
+	Calculator calculator = new Calculator(0.0, 0.0);
 	Scanner scanner = new Scanner(System.in);
-	
-	while(true) {
-	
-	System.out.println("Rechenart eingeben (+ | - | * | / ):");
-	operator = scanner.nextLine();
-	
-	System.out.println("Erste Zahl");
-	firstNumber = Double.parseDouble(scanner.nextLine());
-	
-	System.out.println("Zweite Zahl");
-	secondNumber = Double.parseDouble(scanner.nextLine());
 
-	
-	switch (operator) {
-	case "+": {
-		System.out.println("Ergebnis ist " + calculator.addition(firstNumber, secondNumber));
-	
-		break;
-	}
-	default:
-		throw new IllegalArgumentException("Unexpected value: " + operator);
-	}
-	
-}
+	while (calcRun) {
 
+		System.out.println("Rechenart eingeben (+ | - | * | /)");
+		operator = scanner.nextLine();
+
+		System.out.println("Erste Zahl");
+		firstNumber = Double.parseDouble(scanner.nextLine());
+
+		System.out.println("Zweite Zahl");
+		secondNumber = Double.parseDouble(scanner.nextLine());
+
+		switch (operator) {
+		case "+": {
+			System.out.println("Ergebnis ist " + calculator.addition(firstNumber, secondNumber));
+			break;
+		}
+		case "-": {
+			System.out.println("Ergebnis ist " + calculator.subtraction(firstNumber, secondNumber));
+			break;
+		}
+		case "*": {
+			System.out.println("Ergebnis ist " + calculator.multiplication(firstNumber, secondNumber));
+			break;
+		}
+		case "/": {
+			System.out.println("Ergebnis ist " + calculator.division(firstNumber, secondNumber));
+			break;
+		}
+
+		default:
+			throw new IllegalArgumentException("Unexpected value: " + operator);
+		}
+
+		System.out.println("Exit program? Y/N ?");
+		exitVar = scanner.nextLine();
+		if (exitVar.equals("Y")) { // methode .equals() for string comparing, == is for boolean!!!
+			calcRun = false;
+		} else {
+			calcRun = true;
+		}
+
+	}
 
 }
 
